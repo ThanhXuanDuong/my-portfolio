7824e71e5d232a7d8550e919fe3cdea4ef6f6874
XuanThanhDuong <49959704+ThanhXuanDuong@users.noreply.github.com>
2023-03-03 00:26:23 +0100

    clear form after submit + small changes in #contact

--- frontend/src/components/MyContact.tsx
+++ frontend/src/components/MyContact.tsx
@@ -1,45 +1,90 @@
-import {Box, IconButton, Typography} from "@mui/material";
+import {Box, IconButton, Stack, Typography, useMediaQuery} from "@mui/material";
 import PlaceIcon from '@mui/icons-material/Place';
 import CallIcon from '@mui/icons-material/Call';
 import MailIcon from '@mui/icons-material/Mail';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import theme from "../theme/theme";
 
 export default function MyContact(){
+    const matches = useMediaQuery(theme.breakpoints.up('md'));
     return (
         <Box display="flex"
              flexDirection="column"
              justifyContent="center"
-             alignItems="center"
-             gap="4rem"
+             alignItems={matches? "center":"flex-start"}
+             paddingTop={matches? "2rem":"0"}
+             paddingLeft={matches?"0": "1rem"}
+             gap="2rem"
         >
             <Box display="flex"
-                 flexDirection="column"
+                 flexDirection={matches?"column":"row"}
                  justifyContent="center"
-                 alignItems="center">
+                 alignItems="center"
+                 gap={matches?"0": "2rem"}
+            >
                 <IconButton sx={{backgroundColor:"primary.main"}}>
                     <PlaceIcon sx={{color:"primary.contrastText"}}/>
                 </IconButton>
-                <Typography variant="h6"> Von-Moltke-Bogen 53</Typography>
-                <Typography variant="h6"> 21035 Hamburg</Typography>
+                <Stack>
+                    <Typography variant="body1"> Von-Moltke-Bogen 53</Typography>
+                    <Typography variant="body1"> 21035 Hamburg</Typography>
+                </Stack>
             </Box>
 
             <Box display="flex"
-                 flexDirection="column"
+                 flexDirection={matches?"column":"row"}
                  justifyContent="center"
-                 alignItems="center">
+                 alignItems="center"
+                 gap={matches?"0": "2rem"}
+            >
                 <IconButton sx={{backgroundColor:"primary.main"}}>
                     <CallIcon sx={{color:"primary.contrastText"}}/>
                 </IconButton>
-                <Typography variant="h6"> +49-176-61874750</Typography>
+                <Typography variant="body1"> +49-176-61874750</Typography>
             </Box>
 
             <Box display="flex"
-                 flexDirection="column"
+                 flexDirection={matches?"column":"row"}
                  justifyContent="center"
-                 alignItems="center">
+                 alignItems="center"
+                 gap={matches?"0": "2rem"}
+            >
                 <IconButton sx={{backgroundColor:"primary.main"}}>
                     <MailIcon sx={{color:"primary.contrastText"}}/>
                 </IconButton>
-                <Typography variant="h6"> xuanthanhduong94@gmail.com</Typography>
+                <Typography variant="body1"> xuanthanhduong94@gmail.com</Typography>
+            </Box>
+
+            <Box display="flex"
+                 flexDirection={matches?"column":"row"}
+                 justifyContent="center"
+                 alignItems="center"
+                 gap={matches?"0": "2rem"}
+            >
+                <IconButton sx={{backgroundColor:"primary.main"}}
+                            href="https://www.linkedin.com/in/xuanthanhduong/"
+                            target="blank"
+                >
+                    <LinkedInIcon sx={{color:"primary.contrastText"}}/>
+                </IconButton>
+                <Typography variant="body1">xuanthanhduong</Typography>
+            </Box>
+
+            <Box display="flex"
+                 flexDirection={matches?"column":"row"}
+                 justifyContent="center"
+                 alignItems="center"
+                 marginBottom="2rem"
+                 gap={matches?"0": "2rem"}
+            >
+                <IconButton sx={{backgroundColor:"primary.main"}}
+                            href="https://github.com/ThanhXuanDuong"
+                            target="blank"
+                >
+                    <GitHubIcon sx={{color:"primary.contrastText"}}/>
+                </IconButton>
+                <Typography variant="body1">ThanhXuanDuong</Typography>
             </Box>
         </Box>
     )
